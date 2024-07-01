632117f3105f2d09463062d5764a8fb0a61d1e52
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-01 17:00:16 +0200

    adjust the Login Panel according to the new custom <Input>

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Input from "../Components/input";
 
 const LoginPage = () => {
   return (
@@ -7,21 +8,9 @@ const LoginPage = () => {
       <div className="w-full md:w-[450px]">
         <h1 className="text-white text-center text-4xl mb-9">Login</h1>
         <div className="flex flex-col gap-3 bg-white w-full p-6 min-h-[150px] rounded-xl drop-shadow-xl">
-          <input
-            type="text"
-            placeholder="Enter Name: "
-            className="flex-1 placeholder-gray-300 px-3 py-2 bg-transparent border-2 border-gray-400 rounded-full"
-          ></input>
-          <input
-            type="text"
-            placeholder="Enter Name: "
-            className="flex-2 placeholder-gray-300 px-3 py-2 bg-transparent border-2 border-gray-400 rounded-full"
-          ></input>
-          <input
-            type="text"
-            placeholder="Enter Name: "
-            className="flex-3 placeholder-gray-300 px-3 py-2 bg-transparent border-2 border-gray-400 rounded-full"
-          ></input>
+          <Input type="email" name="Email"></Input>
+          <Input type="password" name="Password"></Input>
+          <Input type="password" name="Password again"></Input>
         </div>
       </div>
     </div>
