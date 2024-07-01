b4dbeee98184342c7c060e131c4023f1656b84b2
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-01 18:22:33 +0200

    Login Componant

---
+++ src/Components/Login.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "./button";
+import Input from "./input";
+
+type Props = {};
+
+const Login = (props: Props) => {
+  return (
+    <div className="w-full md:w-[450px]">
+      <h1 className="text-white text-center text-4xl mb-9">Login</h1>
+      <div className="flex flex-col gap-3 bg-white w-full p-6 min-h-[150px] rounded-xl drop-shadow-xl">
+        <Input type="email" name="Email"></Input>
+        <Input type="password" name="Password"></Input>
+        <Input type="password" name="Password again"></Input>
+        <Button text="login"></Button>
+        <Button text="register" secondary></Button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
