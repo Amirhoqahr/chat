ef738c8eb8fd823c5f18662ddaac1d176fb742eb
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-01 19:15:26 +0200

    add Spinner to the Button

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Spinner from "./Spinner";
 
 type ButtonProps = {
   text: string;
@@ -17,10 +18,13 @@ const Button = ({
 }: ButtonProps) => {
   return (
     <button
-      className={`py-2 px-9 rounded-full text-white border-2 border-white hover:bg-myYellow transition-all hover:drop-shadow-lg ${
+      className={`py-2 px-9 flex justify-center gap-3 rounded-full text-white border-2 border-white hover:bg-myYellow transition-all hover:drop-shadow-lg ${
         secondary ? "bg-myPink" : "bg-myBlue"
-      } ${className}`}
+      } ${className} ${loading && "cursor-wait"}`}
+      onClick={onClick}
+      disabled={loading}
     >
+      {loading && <Spinner />}
       {text}
     </button>
   );
