f0df1a162a68b04fb1b73faaa2c4dd3858a3fcb3
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-05 11:49:57 +0200

    Using Router to route to our pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<LoginPage />}></Route>
+          <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
       <LoginPage />
     </div>
   );
