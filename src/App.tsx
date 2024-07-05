ef175d34dbaded0dd135cdeab73c6520185406bf
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-05 15:58:34 +0200

    Adding new Pages and Adding their Routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./Pages/LoginPage";
+import ChatPage from "./Pages/ChatPage";
+import ListPage from "./Pages/ListPage";
+import ProfilePage from "./Pages/ProfilePage";
+import Layout from "./Pages/Layout";
 
 function App() {
   return (
@@ -9,10 +13,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="" element={<LoginPage />}></Route>
-          <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+          <Route path="/dashboard" element={<Layout />}>
+            <Route index element={<ListPage />}></Route>
+            <Route path="chat" element={<ChatPage />}></Route>
+            <Route path="profile" element={<ProfilePage />}></Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/dashboard" />}></Route>
         </Routes>
       </BrowserRouter>
-      <LoginPage />
     </div>
   );
 }
