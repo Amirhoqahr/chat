368ac57593e6778432f5d975c9809578b432bc27
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-05 11:11:25 +0200

    handle functions of buttons (ready to send datas to the backend)

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -6,17 +6,43 @@ type Props = {};
 
 const Login = (props: Props) => {
   const [login, setLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const LoginButtonFunction = () => {
+    console.log(email, password, confirmPassword);
+  };
+  const RegisterButtonFunction = () => {
+    console.log(email, password, confirmPassword);
+  };
   return (
     <div className="w-full md:w-[450px]">
       <h1 className="text-white text-center text-4xl mb-9">Login</h1>
       <div className="flex flex-col gap-3 bg-white w-full p-6 min-h-[150px] rounded-xl drop-shadow-xl">
-        <Input type="email" name="Email"></Input>
-        <Input type="password" name="Password"></Input>
-        {!login && <Input type="password" name="Password again"></Input>}
+        <Input
+          type="email"
+          name="Email"
+          value={email}
+          onChange={(theInput) => setEmail(theInput.target.value)}
+        ></Input>
+        <Input
+          type="password"
+          name="Password"
+          value={password}
+          onChange={(theInput) => setPassword(theInput.target.value)}
+        ></Input>
+        {!login && (
+          <Input
+            type="password"
+            name="Password again"
+            value={confirmPassword}
+            onChange={(theInput) => setConfirmPassword(theInput.target.value)}
+          ></Input>
+        )}
 
         {login ? (
           <>
-            <Button text="Login"></Button>
+            <Button text="Login" onClick={LoginButtonFunction}></Button>
             <Button
               text="go to the Register page"
               onClick={() => setLogin(false)}
@@ -25,7 +51,7 @@ const Login = (props: Props) => {
           </>
         ) : (
           <>
-            <Button text="Register"></Button>
+            <Button text="Register" onClick={RegisterButtonFunction}></Button>
             <Button
               text="go to the Login page"
               onClick={() => setLogin(true)}
