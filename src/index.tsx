9b0ca353d36da275113e00be8f542e10836dd940
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-07-10 23:32:57 +0200

    add ToastContainer to the index

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import "./Assets/global.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,6 +18,18 @@ root.render(
   // </React.StrictMode>
   <Provider store={store}>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
   </Provider>
 );
 
