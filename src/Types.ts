b0cc355d1d5db52dcd5bd5b5c9a71768c955259a
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-09-15 09:32:47 +0200

    add userType

--- src/Types.ts
+++ src/Types.ts
@@ -1,1 +1,11 @@
 export type setLoadingType = React.Dispatch<React.SetStateAction<boolean>>;
+export type userType = {
+  id: string;
+  username: string;
+  email: string;
+  isOnline: boolean;
+  img: string;
+  bio?: string;
+  creationTime?: string;
+  lastSeen?: string;
+};
