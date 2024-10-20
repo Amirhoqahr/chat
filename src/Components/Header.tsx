6aa8eb2ce0884f069d497891dc43e7a3d53b0910
AmirHossein Ghahramani <130009970+Amirhoqahr@users.noreply.github.com>
2024-10-20 02:13:11 +0200

    change page by clicking on icons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,17 +9,29 @@ import UserHeaderProfile from "./UserHeaderProfile";
 import { useDispatch, useSelector } from "react-redux";
 import { start } from "repl";
 import { RootState } from "../Redux/store";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BE_signOut } from "../Backend/Queries";
 import Spinner from "./Spinner";
 
 type Props = {};
 
 function Header({}: Props) {
+  const goTo = useNavigate();
   const [logoutLoading, setLogoutLoading] = useState(false);
   const dispatch = useDispatch();
   const handleSignOut = () => {
     BE_signOut(dispatch, setLogoutLoading);
+  };
+
+  const setCurrentPage = (page: string) => {
+    localStorage.setItem("superhero-page", page);
+  };
+  const getCurrentPage = () => {
+    return localStorage.getItem("superhero-page");
+  };
+  const handleGoToPage = (page: string) => {
+    goTo("/dashboard/" + page);
+    setCurrentPage(page);
   };
 
   // get the current User from Redux
@@ -32,19 +44,61 @@ function Header({}: Props) {
         className="w-[50px] drop-shadow-md cursor-pointer"
       />
       <div className="flex flex-row-reverse md:flex-row items-center justify-center gap-5 flex-wrap">
-        <AddListBoard />
-        <Icon IconName={BsFillChatFill} ping={true} />
-        <Icon IconName={FiList} />
+        {getCurrentPage() === "chat" ? (
+          <Icon
+            IconName={FiList}
+            onClick={() => {
+              handleGoToPage("list");
+            }}
+          />
+        ) : getCurrentPage() === "profile" ? (
+          <>
+            {/* fragment cuz we have 2 elements to put in */}
+            <Icon
+              IconName={FiList}
+              onClick={() => {
+                handleGoToPage("list");
+              }}
+            />
+            <Icon
+              IconName={BsFillChatFill}
+              ping={true}
+              onClick={() => {
+                handleGoToPage("chat");
+              }}
+            />
+          </>
+        ) : (
+          <>
+            {/* fragment cuz we have 3 elements to put in */}
+            <AddListBoard />
+            <Icon
+              IconName={BsFillChatFill}
+              ping={true}
+              onClick={() => {
+                handleGoToPage("chat");
+              }}
+            />
+            <Icon
+              IconName={FiList}
+              onClick={() => {
+                handleGoToPage("list");
+              }}
+            />
+          </>
+        )}
         <div className="group relative">
           <UserHeaderProfile user={currentUser} />
           <div className="absolute pt-5 hidden group-hover:block w-full min-w-max">
             <ul className="w-full bg-white overflow-hidden rounded-md shadow-md text-gray-700 pt-1 pb-1">
-              <Link
-                to={"/dashboard/profile"}
-                className="block hover:bg-gray-200 py-2 px-4"
+              <p
+                onClick={() => {
+                  handleGoToPage("/profile");
+                }}
+                className="block hover:bg-gray-200 py-2 px-4 hover:cursor-pointer"
               >
                 Profile
-              </Link>
+              </p>
               <Link
                 to={"/"}
                 onClick={() => !logoutLoading && handleSignOut()}
